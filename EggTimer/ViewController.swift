2d58ea53cde8f385ead7d84d3ba31c540d863d76
Ilona Sellenberkova <ilona.sell@gmail.com>
2020-05-19 21:16:25 +0200

    if statement, swift, constants

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  let softTime = 5
+  let mediumTime = 7
+  let hardTime = 12
+
+  @IBAction func hardnessSelected(_ sender: UIButton) {
+//    print(sender.titleLabel?.text!)
+    let hardness = sender.currentTitle!
     
-
-
+    switch hardness {
+    case "Soft":
+      print(softTime)
+    case "Medium":
+      print(mediumTime)
+    case "Hard":
+      print(hardTime)
+    default:
+      print("Error occure")
+    }
+//    if hardness == "Soft" {
+//      print(5)
+//    } else if  hardness == "Medium" {
+//      print(7)
+//    }
+//    else if hardness == "Hard" {
+//      print(12)
+//    }
+  }
 }
